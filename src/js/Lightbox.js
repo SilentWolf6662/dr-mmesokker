e36532d5757531575d53e2054aeb3a54e97822be
SilentWolf6662 <ninjagocola321@gmail.com>
2023-12-14 22:27:43 +0100

    Website done

--- src/js/Lightbox.js
+++ src/js/Lightbox.js
@@ -20,22 +20,26 @@ function currentSlide(n) {
 function showSlides(n) {
 	let i
 
-	const slides = document.getElementsByClassName('slides')
-	const dots = document.getElementsByClassName('cSlide')
-	const captionText = document.getElementById('caption')
+	let slides = document.getElementsByClassName('slides')
+	let dots = document.getElementsByClassName('picture')
+	let captionText = document.getElementById('caption')
 
-	if (n > slides.length) {
+    if (n > slides.length) {
 		slideIndex = 1
 	}
+
 	if (n < 1) {
 		slideIndex = slides.length
 	}
+
 	for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = 'none'
 	}
+
 	for (i = 0; i < dots.length; i++) {
 		dots[i].className = dots[i].className.replace(' active', '')
 	}
+
 	slides[slideIndex - 1].style.display = 'block'
 	dots[slideIndex - 1].className += ' active'
 	captionText.innerHTML = dots[slideIndex - 1].alt
